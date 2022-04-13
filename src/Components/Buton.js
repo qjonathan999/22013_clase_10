bbd13707a3d8c823cbd9dc1f5593a6d5879c7e09
Nathan37 <jhond999@gmail.com>
2022-04-13 14:44:50 -0300

    update

---
+++ src/Components/Buton.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Buton extends React.Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <button>Soy un boton</button>
+        )
+    }
+}
+
+export default Buton
